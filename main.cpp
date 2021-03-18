d3883683b807be9303d0aba2cde85ba779650586
aihoque2 <aihoque2@illinois.edu>
2021-03-17 21:11:54 -0400

    first commit lezzgeddit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/core/cuda.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
